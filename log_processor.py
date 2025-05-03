aba30fbfd9a0a301474451334227cf22e88d2509
Harsh Ranjane <harsh.21910471@viit.ac.in>
2025-05-03 20:01:08 +0200

    Final_changes_DAY3

--- log_processor.py
+++ log_processor.py
@@ -1,15 +1,28 @@
-from utils import normalize_error_lines
-from utils import error_lines
+from utils import normalize_error_lines, read_log_file, extract_error_lines, cleaned_lines, error_lines
+from collections import defaultdict
 
-cleaned_lines = normalize_error_lines(error_lines)
+print("These are the cleaned lines with no timestamps or IDs: ")
+print(cleaned_lines)
 
-def generate_error_fingerprint(cleaned_lines):
-    tokens = [line.split() for line in cleaned_lines]
-    #print("These are the tokens formed :")
-    #print(tokens)
+def generate_error_fingerprint(line):
     stopwords = ['the', 'to', 'could', 'not', 'at', 'in', 'on', 'is', 'of', 'from', 'and']
-    filtered_words = [word for line in tokens for word in line if word.lower() not in stopwords]
-    return filtered_words
+    fingerprint = []
+    tokens = line.split()
+    fingerprint.extend([word for word in tokens if word.lower() not in stopwords])
+    return fingerprint
 
-filtered_words = generate_error_fingerprint(cleaned_lines)
-print(filtered_words)
+
+def cluster_error(cleaned_lines):
+    clusters = defaultdict(list)
+    for line in cleaned_lines:
+        fingerprint = generate_error_fingerprint(line)
+        clusters[tuple(fingerprint)].append(line)
+    return clusters
+
+clusters = cluster_error(cleaned_lines)
+print(clusters)
+
+for fingerprint, logs in clusters.items():
+    print(f"\nFINGERPRINT: {' '.join(fingerprint)}")
+    for log in logs:
+        print(f"  - {log}")
